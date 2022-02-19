0cdfc8b48fa1729deecb757ddc5a1ea73127b163
Константин Васильев <89670253660@mail.ru>
2022-02-19 16:05:53 +0300

    slash

--- yatube/core/tests/test_views.py
+++ yatube/core/tests/test_views.py
@@ -11,4 +11,4 @@ class NotFoundTest(TestCase):
 
     def test_404(self):
         response = self.guest_client.get('/anypage.html')
-        self.assertTemplateUsed(response, 'core\coreĄ.html')
+        self.assertTemplateUsed(response, 'core/coreĄ.html')
