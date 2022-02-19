eeeca40f6319b346b881c65d600f8e31f89c4868
Константин Васильев <89670253660@mail.ru>
2022-02-19 16:02:40 +0300

    coreA

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     # Переменная exception содержит отладочную информацию;
     # выводить её в шаблон пользовательской страницы 404 мы не станем
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
+    return render(request, 'core/coreĄ.html', {'path': request.path}, status=404)
 
 
 def csrf_failure(request, reason=''):
