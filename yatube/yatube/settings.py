13b6d9a9332d4e8a0bdb388f7507b03b5960ebc6
Константин Васильев <89670253660@mail.ru>
2022-06-23 21:22:36 +0300

    Add docker env

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,9 +20,6 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATICFILES_DIRS = [os.path.join(BASE_DIR), 'static']
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
