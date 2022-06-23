f2f34126f39a5cef9636252fd416f322dcb6b4ad
Konstantin8891 <95483744+Konstantin8891@users.noreply.github.com>
2022-06-23 20:10:14 +0300

    Delete yatube/static directory

--- yatube/static/posts/paginator.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import Paginator
-from django.conf import settings
-
-
-def paginator_method(request, posts):
-    paginator = Paginator(posts, settings.POST_AMOUNT)
-    page_number = request.GET.get('page')
-    return paginator.get_page(page_number)
